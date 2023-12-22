1c9be23ab905702962397860ce118a1d8a36be85
Joe Fraser <joe.fraser@thoughtworks.com>
2023-12-22 14:16:53 -0400

    modifying the read function on ingestion

--- etl-animal-data-csv.R
+++ etl-animal-data-csv.R
@@ -1,7 +1,7 @@
 library(readr)
 
 # animal_data <- readr::read_csv('/usr/local/src/myscripts/input_files/animals.csv')
-animal_data <- readr::read_csv('/usr/local/src/myscripts/input_files/animals.csv')
+animal_data <- readr::read_csv('/usr/local/src/myscripts/input_files/animals.csv', lazy = FALSE)
 animal_data$height_in_feet <- round(animal_data$height / 12, 2)
 # readr::write_csv(animal_data,  "/usr/local/src/myscripts/output_files/animals_in_feet.csv")
 readr::write_csv(animal_data,  "/usr/local/src/myscripts/output_files/animals_in_feet.csv")
